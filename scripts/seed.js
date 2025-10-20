3162001e72dba5496a451e65b7ed530853a77c7b
lgegick <lg113@uakron.edu>
2025-10-20 15:51:13 -0400

    Add fountains on floor 1B

--- scripts/seed.js
+++ scripts/seed.js
@@ -11,7 +11,7 @@ async function seed() {
         location TEXT NOT NULL,
         description TEXT,
         flavordescription TEXT,
-        flavorrating REAL,
+        flavorrating TEXT,
         images TEXT[]
       );
     `);
@@ -23,29 +23,69 @@ async function seed() {
 
     const sample = [
       [
-        'Fountain #12',
-        'Engineering Building - 2nd Floor',
-        'Cool and steady flow near the vending machines.',
-        'Tastes crisp with a hint of minerals.',
-        8.5,
-        ['/fountains/12.jpg']
-      ],
-      [
-        'Fountain #5',
-        'Library - Ground Floor',
-        'Older unit, sometimes low pressure but clean.',
-        'Slightly metallic, still refreshing.',
-        7.2,
+        'Fountain #1B',
+        'ASEC - Floor 1B',
+        'By 15 Mech Engineering Research Lab. A button has dislodged from the machine.',
+        'There is a slight aftertaste that doesnt last for too long. Cold. Due to location it will probably be cold year-round. Mineral flavor.',
+        'B-,B+,A,',
         ['/fountains/5.jpg']
       ],
       [
-        'Fountain #21',
-        'Student Union - 1st Floor',
-        'Modern refill station with cold, smooth water.',
-        'Perfectly neutral and refreshing flavor.',
-        9.3,
-        ['/fountains/21.jpg']
-      ]
+        'Fountain #2B',
+        'ASEC - Floor 1B',
+        'Copper erosion and rust around faucet',
+        'Really good and very cold',
+        'A-,A-,B',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #3A B',
+        'ASEC - Floor 1B',
+        'Outside of B227, "splooge" came out of the faucet when first tapped.',
+        'N/A',
+        'N/A',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #3B B',
+        'ASEC - Floor 1B',
+        'Outside of B226 Looks Clean',
+        'N/A',
+        'N/A',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #1A A',
+        'ASEC - Floor 1B',
+        'OWarm and smells like clay and rum. F on looks. Eww looking.',
+        'Slightly metallic, still refreshing.',
+        'B,C+',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #1A B',
+        'ASEC - Floor 1B',
+        'Outside of Zips Electric and bathroom.',
+        'Same as above but slightly worse. I think because we let it run for a bit. We think the water might not be perfectly clear... IT SMELLS THOUGH. Just when we touch metal?',
+        'C+, C+',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #2A B',
+        'ASEC - Floor 1B',
+        'Located outside of B207 by flow',
+        'Decent amount of metal but very cold',
+        'B, C',
+        ['/fountains/5.jpg']
+      ],
+      [
+        'Fountain #2B B',
+        'ASEC - Floor 1B',
+        'Located outside of B207 not by flow',
+        'Even more metal less cold but still cold',
+        'D, B+',
+        ['/fountains/5.jpg']
+      ],
     ];
 
     for (const row of sample) {
